73a26281d3a56b5e5d9d38b2a0a0e6f7dba2e8f9
Unknown11-spec <75899476+Unknown11-spec@users.noreply.github.com>
2022-04-30 23:11:25 +0700

    Update Quiz.cpp

--- Quiz.cpp
+++ Quiz.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main()
 {
-	char keluar;
 	string a;
 	string b;
 	char satu;
@@ -12,6 +11,7 @@ int main()
 	char tiga;
 	char empat;
 	char lima;
+	char keluar;
 	float hasil_1;
 	float hasil_2;
 	float hasil_3;
@@ -29,82 +29,85 @@ int main()
 		exit(0);
 	}
 	if (b == "ya")
-
+	{
 		cout << endl << "1. Penerjemah program komputer ke dalam binary adalah ?" << endl;
-	cout << "a.compiler  b.debugger  c.html" << endl << "Jawaban = ";
-	cin >> satu;
-	if (satu == 'a')
-	{
-		hasil_1 = true;
-		cout << endl;
-	}
-	else
-	{
-		hasil_1 = false;
-		cout << endl;
+		cout << "a.compiler  b.debugger  c.html" << endl << "Jawaban = ";
+		cin >> satu;
+		if (satu == 'a')
+		{
+			hasil_1 = true;
+			cout << endl;
+		}
+		else
+		{
+			hasil_1 = false;
+			cout << endl;
+		}
+
+		cout << "2. C++ ditenukan tahun ?" << endl;
+		cout << "a.1920  b.1980  c.1970" << endl << "Jawaban = ";
+		cin >> dua;
+		if (dua == 'c')
+		{
+			hasil_2 = true;
+			cout << endl;
+		}
+		else
+		{
+			hasil_2 = false;
+			cout << endl;
+		}
+
+		cout << "3. Ekstensi file bahasa pemrograman C++ adalah ?" << endl;
+		cout << "a.py  b.html  c.cpp" << endl << "Jawaban = ";
+		cin >> tiga;
+		if (tiga == 'c')
+		{
+			hasil_3 = true;
+			cout << endl;
+		}
+		else
+		{
+			hasil_3 = false;
+			cout << endl;
+		}
+
+		cout << "4. 10 + 12 ?" << endl;
+		cout << "a.22  b.10  c.00  d.1" << endl << "Jawaban = ";
+		cin >> empat;
+		if (empat == 'a')
+		{
+			hasil_4 = true;
+			cout << endl;
+		}
+		else
+		{
+			hasil_4 = false;
+			cout << endl;
+		}
+
+		cout << "5. 1 + 1 ?" << endl;
+		cout << "a.12  b.3  c.1  d.2" << endl << "Jawaban = ";
+		cin >> lima;
+		if (lima == 'd')
+		{
+			hasil_5 = true;
+			cout << endl;
+		}
+		else
+		{
+			hasil_5 = false;
+			cout << endl;
+		}
+
+		int benar = hasil_1 + hasil_2 + hasil_3 + hasil_4 + hasil_5;
+		int salah = 5 - benar;
+		cout << "Nama = " << a << endl;
+		cout << "Benar = " << benar << endl;
+		cout << "Salah = " << salah << endl;
+		cout << "Nilai = " << 20 * benar << endl << endl;
+		_getch();
 	}
 
-	cout << "2. C++ ditenukan tahun ?" << endl;
-	cout << "a.1920  b.1980  c.1970" << endl << "Jawaban = ";
-	cin >> dua;
-	if (dua == 'c')
-	{
-		hasil_2 = true;
-		cout << endl;
-	}
-	else
-	{
-		hasil_2 = false;
-		cout << endl;
-	}
-
-	cout << "3. Ekstensi file bahasa pemrograman C++ adalah ?" << endl;
-	cout << "a.py  b.html  c.cpp" << endl << "Jawaban = ";
-	cin >> tiga;
-	if (tiga == 'c')
-	{
-		hasil_3 = true;
-		cout << endl;
-	}
-	else
-	{
-		hasil_3 = false;
-		cout << endl;
-	}
-
-	cout << "4. 10 + 12 ?" << endl;
-	cout << "a.22  b.10  c.00  d.1" << endl << "Jawaban = ";
-	cin >> empat;
-	if (empat == 'a')
-	{
-		hasil_4 = true;
-		cout << endl;
-	}
-	else
-	{
-		hasil_4 = false;
-		cout << endl;
-	}
-
-	cout << "5. 1 + 1 ?" << endl;
-	cout << "a.12  b.3  c.1  d.2" << endl << "Jawaban = ";
-	cin >> lima;
-	if (lima == 'd')
-	{
-		hasil_5 = true;
-		cout << endl;
-	}
-	else
-	{
-		hasil_5 = false;
-		cout << endl;
-	}
-
-	int benar = hasil_1 + hasil_2 + hasil_3 + hasil_4 + hasil_5;
-	int salah = 5 - benar;
-	cout << "Nama = " << a << endl;
-	cout << "Benar = " << benar << endl;
-	cout << "Salah = " << salah << endl;
-	cout << "Nilai = " << 20 * benar << endl << endl;
-	_getch();
+	
 }
